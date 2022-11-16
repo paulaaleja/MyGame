11c5158e52b6d9c377b66b34c79a0f31c3fdf9d1
paula garcia <paulagarcia1306@gmail.com>
2022-11-16 00:33:31 -0300

    first commit

---
+++ screens/ResultScreen.js
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import Card from "../components/Card";
+import lose from "../assets/images/loser.gif";
+import win from "../assets/images/win.gif";
+
+const ResultScreen = ({ result }) => {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    handleImage();
+  }, []);
+
+  const handleImage = () => {
+    if (result === "win") {
+      setImage(win);
+      return;
+    }
+    setImage(lose);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Card>
+        <Text>{`you ${result}`}</Text>
+      </Card>
+      <Image style={styles.imageContainer} source={image} />
+    </View>
+  );
+};
+
+export default ResultScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    height: 320,
+    width: 320,
+    marginTop: 50,
+  },
+});
